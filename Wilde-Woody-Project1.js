f64b921ceb607aff2cd4e57850f10001f0a1aa87
Woody Wilde <wawilde@fullsail.edu>
2012-09-25 07:37:40 -0400

    Added beginning of JavaScript File-Project 1
    Project 1 started

---
+++ Wilde-Woody-Project1.js
@@ -0,0 +1,37 @@
+/*
+ *Woody Wilde
+ * SDI1210
+ * Project 1
+ */
+
+//declaring variables
+var boyName = "Bobby";
+var owl = "\"let's find out.\"";
+var sucker = "Tootsie Roll TootsiePop";
+var stick = true;
+var licks = 5;
+
+//variable output via console.log
+console.log(boyName +"Ask's Mr. Owl, \"how many lick's does it take to get to the center of a tootsie roll tootsie pop?\"");
+console.log("Mr. Owl says \"" + owl + "\"");
+console.log("Mr. Owl counts up to " + licks + " and says, " + licks);
+console.log(boyName + " asked Mr.owl if he still had the stick, and Mr. Owl said this was " + stick);
+console.log("Now all " + boyName +" will eat is a " + sucker + ".");
+
+
+//code execution
+if (stick === true) {
+	if (licks === 3) {
+		console.log("The owl has the " + sucker + "and it takes " + licks + "licks to get to the center!");
+      } else {
+		console.log(boyName + " has the " + sucker +".");		
+         {
+      	console.log("I think it takes more than " +licks + " licks to get to the center"); 
+};
+
+
+
+
+
+
+
