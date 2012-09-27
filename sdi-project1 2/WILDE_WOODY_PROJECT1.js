afbc2d0225d3b6a9d19ff175f5d54f4c38edfb4f
Woody Wilde <wawilde@fullsail.edu>
2012-09-27 08:41:26 -0400

    FINISHED i think...
    took mr lewis's advice and made the needed changes to make everything
    work

--- sdi-project1 2/WILDE_WOODY_PROJECT1.js
+++ sdi-project1 2/WILDE_WOODY_PROJECT1.js
@@ -8,16 +8,32 @@
 
 //declaring variables
 var boyName = "Bobby";
-var owl = "\"let's find out.\"";
+var owlSays = "\"let's find out.\"";
 var sucker = "Tootsie Roll TootsiePop";
-var stick = true;
-var licks = 5;
+var hasStick = true;
+var numOfLicks = 3;
 
 //variable output via console.log
 console.log(boyName + "Ask's Mr. Owl, \"how many lick's does it take to get to the center of a tootsie roll tootsie pop?\"");
-console.log("Mr. Owl says \"" + owl + "\"");
-console.log("Mr. Owl counts up to " + licks + " and says, " + licks);
-console.log(boyName + " asked Mr.owl if he still had the stick, and Mr. Owl said this was " + stick);
-console.log("Now all " + boyName + " will eat is a " + sucker + ".");
+console.log("Mr. Owl says \"" + owlSays + "\"");
+console.log("Mr. Owl begins licking the " + sucker + " and says, \" It should take about " + numOfLicks  + ".\"");
+console.log(boyName + " asked Mr.owl if he still had the stick, and Mr. Owl said this was " + hasStick);
+
 
 //code execution
+if (boyName === "Bobby") {
+        if(numOfLicks === 3){
+        numOfLicks = numOfLicks + 1;
+        console.log("The owl takes a bite on lick number " + numOfLicks + " and says 3");
+    } else {
+        console.log("Hey! You ate my sucker!");
+    }
+} else {
+    if (hasStick === true ){
+        consolelog.("Mr. Owl still has the sucker stick.");
+    } else {
+      console.log("Mr. Owl does not has the sucker stick");  
+    };
+    
+};
+console.log("Now all " + boyName + " will eat is a " + sucker + ".");
