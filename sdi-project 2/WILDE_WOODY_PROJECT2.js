5400aab5cdee292169c873f16aaa2571b3fc1714
Woody Wilde <wawilde@fullsail.edu>
2012-10-02 13:25:32 -0400

    Making changes suggested by Mr. Lewis
    changed procedure - remove the return and one argument to be consistent
    with flow chart

--- sdi-project 2/WILDE_WOODY_PROJECT2.js
+++ sdi-project 2/WILDE_WOODY_PROJECT2.js
@@ -10,22 +10,26 @@
 var strName = "Woody";
 var numAge = 41;
 var arrStates = [ Florida, Georgia, Tennesee, Kentucky, Indiana ];
+var arg1 = 23;
+var arg2 = 19;
+var funcNum = arg1 + arg2;
 
 //procedure
-var addition = function (num1, num2){
-    var sum = num1 + num2;
-    console.log("The total of " + num1 +" and " + num2 + " is " + sum);
-    return sum;
+var addition = function (num1){
+    var sum = num1 + 5;
+        if (sum >= 10) {
+        console.log("The total of " + num1 + " plus 5 is : " + sum);
+    } else {
+    console.log("When " + num1 + " is added to 5 it totals " + sum + ".");
+    
 };
 
 //boolean function
-var arg1 = 23;
-var arg2 = 19;
-var funcNum = arg1 + arg2;  
+ 
     var booFunc = function (arg1, arg2){
-        if (arg1 > arg2 === true){ 
+        if (arg1 > arg2){ 
             console.log(arg1 + " is greater than " + arg2);
-            if (arg1 < arg2 === false){
+            if (arg1 < arg2){
                 console.log("I think " + arg2 + " is less than " + arg1);  
             } else { 
                 console.log("Aha! Proof that " + arg2 + " is greater than " + arg1);
