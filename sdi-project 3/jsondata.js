d49c290d3a3ad94b44da4eef00126e9922c84834
Woody Wilde <wawilde@fullsail.edu>
2012-10-09 03:15:56 -0400

    started creation of my JSON data and added the script line to my html

---
+++ sdi-project 3/jsondata.js
@@ -0,0 +1,18 @@
+/*
+ * JSON data
+ * Woody Wilde
+ * Project 3
+ * SDI-1210
+ */
+
+var cast = {
+    "name":        "Zaphod Beeblbrox",
+    "occupation":  "Universal President",
+    "ship":        "The Heart of Gold",
+
+},
+{
+    "name": "Ford Prefect",
+    "occupation": "Blocking Bulldozers",
+    "ship": ""
+},
