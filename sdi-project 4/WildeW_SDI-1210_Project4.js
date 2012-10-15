e7d10ca634ac1f28c4f23e57433b2965f12df2d9
Woody Wilde <wawilde@fullsail.edu>
2012-10-15 03:15:23 -0400

    prepping for work on project 4

--- sdi-project 4/WildeW_SDI-1210_Project4.js
+++ sdi-project 4/WildeW_SDI-1210_Project4.js
@@ -1,1 +1,29 @@
-alert("JavaScript works!");
+/*
+ *Woody Wilde
+ *SDI-12010
+ *Project 4
+ *oct.13,2012
+ *Function Library
+ */
+/******************STRING FUNCTIONS**************************************/
+//Does a string follow a 123-456-7890 pattern like a phone number?
+var phoneNum = function ( ) {
+    
+};
+//Does a string follow anaaa@bbb.ccc pattern like an email address?
+var emailFun = function ( ) {
+    
+};
+//Is the string a URL? (Does it start with http: or https:?)
+var isUrl = function () {
+    
+};
+//Title-case a string (split into words, then uppercase the first letter of each word)
+
+//Given a string that is a list of things separated by a given string, as well as another string separator, return a string with the first separator changed to the second: "a,b,c" + "," + "/" ? "a/b/c".
+
+/*****************************STRING FUNCTION CALLS**********************************/
+
+console.log(phoneNum());
+console.log(emailFun());
+console.log(isUrl());
