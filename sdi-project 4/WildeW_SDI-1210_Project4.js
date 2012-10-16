5809c10c7592dc4fcdd364e47578c4ba062334b3
Woody Wilde <wawilde@fullsail.edu>
2012-10-16 03:07:18 -0400

    EMAIL CHECKER FUNCTION WORKING...

--- sdi-project 4/WildeW_SDI-1210_Project4.js
+++ sdi-project 4/WildeW_SDI-1210_Project4.js
@@ -11,7 +11,7 @@ var myLibrary = function ( ) {
 
 
 /******************STRING FUNCTIONS**************************************/
-//Does a string follow a 123-456-7890 pattern like a phone number?
+//Does a string follow a 123-456-7890 pattern like a phone number?  ----  <---WORKING DO NOT MESS WITH
     var chkPhone = function (testNumber) {
             
             var phoneNumber = testNumber;
@@ -32,11 +32,21 @@ var myLibrary = function ( ) {
             
     };
 
-/*Does a string follow anaaa@bbb.ccc pattern like an email address?
-    var emailFun = function ( val ) {
+//Does a string follow anaaa@bbb.ccc pattern like an email address?  ----  <---WORKING DO NOT MESS WITH
+    var emailFun = function ( checkEmail ) {
         
-    
-    };*/
+        var emailAddress = checkEmail
+        
+            var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,4}$/// THESE ARE THE CHARACTERS AND STRING IT WILL MARK AS TRUE.
+            
+                if (emailPattern.test(emailAddress)) {              // TESTING checkEmail TO SEE IF IT MATCHES emailPattern.
+            
+                    return true;    //RETURNS TRUE IF IT IS A VALID EMAIL
+                } else {
+                    return false;   //RETURNS FALSE IF IT IS NOT A VALID EMAIL 
+            };
+             
+    };
 //Is the string a URL? (Does it start with http: or https:?)
     var isUrl = function () {
     
@@ -47,12 +57,17 @@ var myLibrary = function ( ) {
         
         return {
               "chkPhone": chkPhone,
-              
+              "emailFun": emailFun,
+              ""
             };
 };
 var myStuff = new myLibrary();
 /*****************************STRING FUNCTION CALLS**********************************/
-console.log("Is this a phone number ? " + myStuff.chkPhone("Woody"));          //SHOULD GIVE A FALSE OUTPUT
+//PHONE NUMBER CHECK
 console.log("Is this a phone number ? " + myStuff.chkPhone("317-490-7081"));   //SHOULD GIVE A TRUE OUPUT
-//console.log(emailFun());
-//console.log(isUrl());
+console.log("Is this a phone number ? " + myStuff.chkPhone("Woody"));          //SHOULD GIVE A FALSE OUTPUT
+//EMAIL CHECK
+console.log("Is this a valid email address ? " + myStuff.emailFun("wawilde@fullsail.edu"));    //SHOULD GIVE A TRUE OUTPUT
+console.log("Is this a valid email address ? " + myStuff.emailFun("123@458")); //SHOULD GIVE A FALSE OUTPUT
+//URL CHECK
+console.log(isUrl());
