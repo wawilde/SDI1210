5fb026a8e312ce5fd1db4acd220026c152e8b606
Woody Wilde <wawilde@fullsail.edu>
2012-10-17 03:33:32 -0400

    2 MORE FUNCTIONS WORKING ABOUT FIFTY MORE TO GO.
    I believe i have all of my String functions working

--- sdi-project 4/WildeW_SDI-1210_Project4.js
+++ sdi-project 4/WildeW_SDI-1210_Project4.js
@@ -28,7 +28,7 @@ var myLibrary = function ( ) {
                     
                     return false;    //FALSE IF IT IS NOT A PHONE NUMBER
                     
-            }
+            };
             
     };
 
@@ -37,7 +37,7 @@ var myLibrary = function ( ) {
         
         var emailAddress = checkEmail
         
-            var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,4}$/// THESE ARE THE CHARACTERS AND STRING IT WILL MARK AS TRUE.
+            var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,4}$/// THESE ARE THE CHARACTERS AND STRING IT WILL LOOK FOR.
             
                 if (emailPattern.test(emailAddress)) {              // TESTING checkEmail TO SEE IF IT MATCHES emailPattern.
             
@@ -47,18 +47,62 @@ var myLibrary = function ( ) {
             };
              
     };
-//Is the string a URL? (Does it start with http: or https:?)
-    var isUrl = function () {
+//Is the string a URL? (Does it start with http: or https:?)    ---- <---WORKING DO NOT MESS WITH!
+    var isUrl = function checkUrl(testUrl) {
+        var url = testUrl
+        
+            var re = /^(http[s]?:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,5}[\.]{0,1}/;    //THESE ARE THE AVBAILABLE CHARACTER FOR THE STRING 
     
-    };
+                var urlString = re.test(url);    //CHECKING THE STRING TO SEE IF IT IS AN URL
+                
+            if (url.charAt(4) == "s") {       //THIS CHECKS THE FOURTH POSTION IN THE STRING FOR AN "S" TO SEE IF URL USES HTTPS:
+                    
+                    return true;              //IF THE URL STRING IS SECURED THIS WILL GIVE A TRUE OUTPUT
+         } 
+            
+             if (url.charAt(4) == ":") {     //IF THE URL STRING DOES NOT CONTAIN AN "S" BUT HAS A ":" INSTEAD
+            
+                    return false;            //SHOULD RETURN A FALSE OUTPUT IF URL IS NORMAL AND NOT "HTTPS"
+        }
+}
 //Title-case a string (split into words, then uppercase the first letter of each word)
+    var titleCase = function fixCase(fixThis) {
+        
+        String.prototype.toProperCase = function () {           //CALLS PROTOTYPE STRING FUNCTION
+ 
+        return this.replace(/\w\S*/g, function (txt) {          //TELLS WHICH LETTER(S) SHOULD BE UPPERCASE
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();       //DEFINES WHAT CHARACTER SPACES SHOULD BE CHANGED
+            });
+        };
+        return fixThis.toProperCase();          //RETURNS TITLECASE VARIABLE VALUE WITH FIRST LETTER OF ANY WORDS CAPITALIZED
+    }
 
 //Given a string that is a list of things separated by a given string, as well as another string separator, return a string with the first separator changed to the second: "a,b,c" + "," + "/" ? "a/b/c".
-        
+     var changeString = function changeSeparator(changeThis) {  //<--- WORKING DO NOT MESS WITH
+            
+            if (changeThis == "a,b,c,") {       
+                
+                changeThis = "a/b/c/";      // CHANGES THE STRING "a,b,c"
+                
+                return changeThis;          //RETURNS THE NEW VALUE OF "a/b/c/"
+            
+            } else {    
+            
+                changeThis = "1.2.3.";      //CHANGES THE VALUE OF "1,2,3,"
+            
+                return changeThis;          //RETURNS THE VALUE OF "1.2.3."
+    };
+
+}
+
+//returns for each function call
         return {
               "chkPhone": chkPhone,
               "emailFun": emailFun,
-              ""
+              "isUrl":    isUrl,
+              "changeString": changeString,
+              "titleCase": titleCase
+              
             };
 };
 var myStuff = new myLibrary();
@@ -70,4 +114,29 @@ console.log("Is this a phone number ? " + myStuff.chkPhone("Woody"));          /
 console.log("Is this a valid email address ? " + myStuff.emailFun("wawilde@fullsail.edu"));    //SHOULD GIVE A TRUE OUTPUT
 console.log("Is this a valid email address ? " + myStuff.emailFun("123@458")); //SHOULD GIVE A FALSE OUTPUT
 //URL CHECK
-console.log(isUrl());
+console.log("Is this a https url? " + myStuff.isUrl("https://fullsail.edu") ); //SHOULD GIVE A TRUE OUTPUT
+console.log("Is this a https url ? " + myStuff.isUrl("http://www.google.com") );  //SHOULD GIVE A FALSE OUTPUT
+//TITLE CASE CHANGE
+console.log(myStuff.titleCase("the quick brown fox."));             //CHANGES THE FIRST LETTER OF EACH WORD TO A CAPITAL LETTER
+//STRING CHANGE
+console.log("This function just changed a,b,c, to : " + myStuff.changeString("a,b,c,") );   //CHANGES THE "," TO A "/" DISPLAYS AS a/b/c
+console.log("This function just changed 1,2,3, to : " + myStuff.changeString("1,2,3,") );   //CHANGES THE "," TO A "." DISPLAYS AS 1.2.3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
